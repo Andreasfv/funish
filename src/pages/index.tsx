fe9ea2ba1d4db5dfa530cb664d14946fbf605301
Andreasfv <andreas.vangen@gmail.com>
2023-03-25 18:06:40 +0100

    feat(googleLogin): idk

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,20 +43,6 @@ const Home: NextPage = () => {
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-1 md:gap-8">
-            {admin && (
-              <Link
-                className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-                href={`${me?.data?.user?.organizationId ?? ""}/organizations`}
-              >
-                {t(`common.organization`)}
-              </Link>
-            )}
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href={"/dashboard"}
-            >
-              {"Dashboard"}
-            </Link>
           </div>
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
